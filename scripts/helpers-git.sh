cbaa036586198c95cb70548f3b5480f305bcd5e7
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-24 10:57:37 -0800

    Update helpers-git.sh

--- scripts/helpers-git.sh
+++ scripts/helpers-git.sh
@@ -24,7 +24,7 @@ function configure_split_remote_URLs_by_public_GitHub_repo_name() {
   local github_repo_name="$1"
   
   # Configure split remote URLs (HTTPS fetch / SSH push)
-  report_action_taken "Configure local clone of ${github_repo_name} to fetch with HTTPS but pull with SSH"
+  report_action_taken "Configure local clone of ${github_repo_name} to fetch with HTTPS but push with SSH"
   report_adjust_setting "Configure ${github_repo_name} to fetch via HTTPS"
   git remote set-url origin https://github.com/jimratliff/"${github_repo_name}".git ; success_or_not
   report_adjust_setting "Configure ${github_repo_name} to push via SSH"
