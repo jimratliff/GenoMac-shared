9905b0316e64ff9c6e9a110d4c73599dc4688a92
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-02-20 16:00:30 -0800

    Update helpers-migrate.sh

--- scripts/helpers-migrate.sh
+++ scripts/helpers-migrate.sh
@@ -3,21 +3,40 @@
 ############### Helpers related to migrating state for GenoMac
 
 _migrate_states() {
+  # Helper for migration of state(s) for either 'system' or 'user' scope.
+  #
+  # Takes two positional arguments and one option followed by a sequence of strings.
+  #
+  # $1: scope, either 'system' or 'user'
+  # $2: the ID of a particular migration. This is a string that begins with MIGRATION_STATE_PREFIX
+  #     The migration ID itself refers to a state in the state spaced specified by scope ($1).
+  #     If this state does not exist, the remainder of this function runs, at the end of which this 
+  #     state is created.
+  #     If this state already exists, then this migration had already been completed, and this function
+  #     exits normally.
+  # --delete: Currently, this is mandatory and the only available option.
+  #           It must be followed by a sequence of one or more strings, each of which refers to a
+  #           state within the state space specified by scope ($1).
+  #           These are the states to be deleted as part of this migration.
+  
   report_start_phase_standard
   local scope="$1"
   local migration_id="$2"
   local -a states_to_delete
 
   # Validate scope
-  if ! _validate_scope "$scope"; then
-    report_fail "migrate_states: invalid scope: '${scope}'"
-    return 1
-  fi
+  _validate_scope "$scope" || return 1
 
   # Validate migration_id begins with $MIGRATION_STATE_PREFIX
   if [[ "$migration_id" != "${MIGRATION_STATE_PREFIX}"* ]]; then
-    report_fail "migration_id (${migration_id}) must begin with '${MIGRATION_STATE_PREFIX}': '$migration_id'"
+    report_fail "migration_id (“${migration_id}”) must begin with '${MIGRATION_STATE_PREFIX}': '$migration_id'"
     return 1
+  fi
+
+  if _test_state "${migration_id}" "${scope}"; then
+    report_action_taken "Skipping this migration (“${migration_id}”) because it was completed in the past"
+    report_end_phase_standard
+    return 0
   fi
 
   # Parse options
@@ -44,5 +63,10 @@ _migrate_states() {
     _delete_state "$state" "$scope"
   done
 
+  # Create state for this migration_ID so that the migration will never be executed again for this Mac-user combination
+  _set_state "${migration_id}" "${scope}"
+  
   report_end_phase_standard
 }
+
+
