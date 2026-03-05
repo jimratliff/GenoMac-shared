13c2d09d4c641bdd4f407ce555f7f4afbb2fdcaf
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-03-05 15:13:32 -0800

    Remove DEBUG prints

--- scripts/helpers-copying.sh
+++ scripts/helpers-copying.sh
@@ -103,13 +103,9 @@ function copy_resource_between_local_directories() {
     report_action_taken "Unzip ${source_path} to temp directory"
     unzip -q "$source_path" -d "$tmp_dir" -x '__MACOSX/*' ; success_or_not
 
-    report "DEBUG: Back from unzip. Here’s a listing of what I have:"
-    ls -la "$tmp_dir"
-
     # Locate the single top-level directory inside the zip
     # source_path=${tmp_dir}/*(N)
     source_path=$(find "$tmp_dir" -mindepth 1 -maxdepth 1 -type d)
-    report "DEBUG: source_path=${NEWLINE}[$source_path]"
 
     if [[ ! -d "$source_path" ]]; then
       report_fail "Expected a single top-level directory inside zip; got: $(ls "$tmp_dir")"
