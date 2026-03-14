6bd083f334acbb2b9bbc3b0f502b5edc71d280a1
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-03-13 20:31:13 -0700

    Remove vestigial, deprecated code

--- scripts/helpers-interactive.sh
+++ scripts/helpers-interactive.sh
@@ -79,18 +79,6 @@ function get_confirmed_answer_to_question() {
   echo "$answer"
 }
 
-# function open_privacy_panel_for_full_disk_permissions() {
-#   # This is DEPRECATED because it was used only in conjunction with launch_app_and_prompt_user_to_act()
-#   # which now has its own --open option, so this operation can be built into the call.
-#   open "$SYSTEM_SETTINGS_PRIVACY_SECURITY_PANEL_URL_FULL_DISK"
-# }
-
-# function open_wallpaper_panel() {
-#   # This is DEPRECATED because it was used only in conjunction with launch_app_and_prompt_user_to_act()
-#   # which now has its own --open option, so this operation can be built into the call.
-#   open "$SYSTEM_SETTINGS_WALLPAPER_PANEL_URL"
-# }
-
 function show_file_using_quicklook() {
   # Shows a file using Quick Look, where that file is supplied by a path string in the only argument
   #
