cd3ac714ad006984ed35a715cc029b7f9006ab30
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-05-21 15:06:34 -0700

    Update helpers-state.sh

--- scripts/helpers-state.sh
+++ scripts/helpers-state.sh
@@ -389,6 +389,23 @@ function construct_state_string_for_user_attribute(){
   print -- "$state_string"
 }
 
+function _test_state_for_user_attribute(){
+  # $1: user_short_name: The user to whom the attribute belongs
+  # $2: attribute_name
+  # $3: the "scope," either 'system' or 'user' depending on whether this state characterizes 
+  # 	  (a) the entire 'system' (e.g., that Mac) or instead (b) characterizes a particular 'user'
+
+  local user_short_name="${1:?missing/empty user_short_name}"
+  local attribute_name="${2:?missing/empty attribute_name}"
+  local scope="${3:?missing/empty scope}"
+  local state_string
+  
+  state_string="$(construct_state_string_for_user_attribute "$user_short_name" "$attribute_name")"
+  
+  # The following call to _test_state must be the last command of this function, because its return value is crucial to the caller
+  _test_state "${state_string}" "$scope"
+}
+
 function _set_state_for_user_attribute(){
   # $1: user_short_name: The user to whom the attribute belongs
   # $2: attribute_name
