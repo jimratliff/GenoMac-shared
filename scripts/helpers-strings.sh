40d2285deaa74e72379e22bb24be2d72682178ad
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-05-28 15:39:44 -0700

    Update helpers-strings.sh

--- scripts/helpers-strings.sh
+++ scripts/helpers-strings.sh
@@ -7,8 +7,8 @@ function sanitize_filename() {
 }
 
 function content_between_delimiters(){
-  # Prints the substring of string_to_parse that lies between left_delimiter
-  # and right_delimiter.
+  # Prints the nonempty substring of string_to_parse that lies between
+  # left_delimiter and right_delimiter.
   #
   # Arguments:
   #   $1: string_to_parse
@@ -16,7 +16,8 @@ function content_between_delimiters(){
   #   $3: right_delimiter
   #
   # Returns:
-  #   0 if both delimiters are found in the expected order
+  #   0 if both delimiters are found in the expected order and the
+  #     inbetween content is nonempty.
   #   1 otherwise
 
   local string_to_parse="${1:?missing/empty string_to_parse}"
@@ -24,6 +25,7 @@ function content_between_delimiters(){
   local right_delimiter="${3:?missing/empty right_delimiter}"
 
   local after_left
+  local content
 
   if [[ "$string_to_parse" != *"$left_delimiter"* ]]; then
     report_fail "The string to parse doesn’t contain the left delimiter “${left_delimiter}”.${NEWLINE}String to parse: ${string_to_parse}"
@@ -37,5 +39,12 @@ function content_between_delimiters(){
     return 1
   fi
 
-  print -r -- "${after_left%%"$right_delimiter"*}"
+  content="${after_left%%"$right_delimiter"*}"
+
+  if [[ -z "$content" ]]; then
+    report_fail "Empty content between the left delimiter “${left_delimiter}” and the right delimiter “${right_delimiter}”.${NEWLINE}String to parse: ${string_to_parse}"
+    return 1
+  fi
+
+  print -r -- "$content"
 }
