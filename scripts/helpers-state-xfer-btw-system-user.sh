ed5f88316c6aa3dbcb3d6001ca3c9c2f226e1261
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-05-30 11:54:54 -0700

    Update helpers-state-xfer-btw-system-user.sh

--- scripts/helpers-state-xfer-btw-system-user.sh
+++ scripts/helpers-state-xfer-btw-system-user.sh
@@ -76,7 +76,7 @@ function construct_state_string_for_user_in_need_of_initial_config(){
 }
 
 ##############################
-# State functions for user attributes
+# State functions for user attributes and user class
 #
 # User-attribute states are system-scoped states because, at the time they are created, the
 # relevant user doesn’t yet have a home directory.
@@ -85,18 +85,27 @@ function set_system_state_for_user_attribute(){
   # Set system-scoped state asserting given user has given attribute.
 
   report_start_phase_standard
-  
   local short_name="${1:?missing short name}"
   local attribute_name="${2:?missing attribute name}"
-  
-  # Hint: GENOMAC_STATE_USER_ATTRIBUTE_PREFIX="USER_ATTRIBUTE"
-  local state_string_prefix="$GENOMAC_STATE_USER_ATTRIBUTE_PREFIX"
-  
-  local state_string
-  state_string="${state_string_prefix}${GENOMAC_STATE_STRING_DELIMITER_A}${short_name}${GENOMAC_STATE_STRING_DELIMITER_B}${attribute_name}${GENOMAC_STATE_STRING_DELIMITER_C}"
-  set_genomac_system_state "$state_string"
+
+  _set_state_for_user_attribute "$short_name" "$attribute_name" "system"
 
   report_end_phase_standard
+}
+
+function _set_state_for_user_attribute(){
+  # $1: user_short_name: The user to whom the attribute belongs
+  # $2: attribute_name
+  # $3: the "scope," either 'system' or 'user' depending on whether this state characterizes 
+  # 	  (a) the entire 'system' (e.g., that Mac) or instead (b) characterizes a particular 'user'
+
+  local user_short_name="${1:?missing/empty user_short_name}"
+  local attribute_name="${2:?missing/empty attribute_name}"
+  local scope="${3:?missing/empty scope}"
+  local state_string
+  
+  state_string="$(construct_state_string_for_user_attribute "$user_short_name" "$attribute_name")"
+  _set_state "${state_string}" "$scope"
 }
 
 function construct_state_string_for_user_attribute(){
@@ -111,7 +120,7 @@ function construct_state_string_for_user_attribute(){
   local attribute_name="$2"
   local state_string
 
-  state_string="${GENOMAC_STATE_USER_ATTRIBUTE_PREFIX}${user_short_name}_${attribute_name}"
+  state_string="${GENOMAC_STATE_USER_ATTRIBUTE_PREFIX}${GENOMAC_STATE_STRING_DELIMITER_A}${short_name}${GENOMAC_STATE_STRING_DELIMITER_B}${attribute_name}${GENOMAC_STATE_STRING_DELIMITER_C}"
 
   print -- "$state_string"
 }
@@ -133,17 +142,32 @@ function _test_state_for_user_attribute(){
   _test_state "${state_string}" "$scope"
 }
 
-function _set_state_for_user_attribute(){
-  # $1: user_short_name: The user to whom the attribute belongs
-  # $2: attribute_name
-  # $3: the "scope," either 'system' or 'user' depending on whether this state characterizes 
-  # 	  (a) the entire 'system' (e.g., that Mac) or instead (b) characterizes a particular 'user'
+function set_system_state_for_user_class(){
+  # Set system-scoped state asserting given user has given user class.
 
-  local user_short_name="${1:?missing/empty user_short_name}"
-  local attribute_name="${2:?missing/empty attribute_name}"
-  local scope="${3:?missing/empty scope}"
-  local state_string
-  
-  state_string="$(construct_state_string_for_user_attribute "$user_short_name" "$attribute_name")"
-  _set_state "${state_string}" "$scope"
+  report_start_phase_standard
+  local short_name="${1:?missing short name}"
+  local user_class="${2:?missing user class}"
+
+  _set_state_for_user_attribute "$short_name" "$user_class" "system"
+
+  report_end_phase_standard
 }
+
+function construct_state_string_for_user_class(){
+  # Constructs the state string for a user class of the form: "USER_ATTRIBUTE_shortname_attributename"
+  # Hint: GENOMAC_STATE_USER_ATTRIBUTE_PREFIX="USER_ATTRIBUTE_"
+  # $1: user_short_name: The user to whom the attribute belongs
+  # $2: user_class
+  #
+  # The result is printed to stdout
+
+  local user_short_name="$1"
+  local user_class="$2"
+  
+  local state_string
+  state_string="${GENOMAC_STATE_USER_CLASS_PREFIX}${GENOMAC_STATE_STRING_DELIMITER_A}${short_name}${GENOMAC_STATE_STRING_DELIMITER_B}${user_class}${GENOMAC_STATE_STRING_DELIMITER_C}"
+  print -- "$state_string"
+  
+}
+
