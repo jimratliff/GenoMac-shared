3ac6a124451a2b37933d82a2d7e7ce24140ac652
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-08-29 08:08:42 -0700

    Update assign_common_environment_variables.sh

--- scripts/assign_common_environment_variables.sh
+++ scripts/assign_common_environment_variables.sh
@@ -55,18 +55,6 @@ GENOMAC_SHARED_DOCS_TO_DISPLAY_DIRECTORY="${GENOMAC_SHARED_RESOURCE_DIRECTORY}/d
 GENOMAC_SYSTEM_LOCAL_DIRECTORY="$HOME/.genomac-system"
 GENOMAC_USER_LOCAL_DIRECTORY="$HOME/.genomac-user"
 
-############### BEGIN DEBUG
-print -u2 -- "DEBUG before prefix: ERR_EXIT=${options[errexit]}"
-############### END DEBUG
-
-############### BEGIN DEBUG
-print -u2 -- "DEBUG testing ERR_EXIT context"
-false
-print -u2 -- "DEBUG survived false"
-############### END DEBUG
-
-
-
 # Resolve once (don’t recompute if already set by the environment)
 HOMEBREW_PREFIX="$(get_homebrew_prefix)"
 
