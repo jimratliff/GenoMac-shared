c498bf7314369c6aa554891ac4759d022361afd0
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-09-05 09:55:38 -0700

    Update helpers-files.sh

--- scripts/helpers-files.sh
+++ scripts/helpers-files.sh
@@ -31,6 +31,54 @@ function file_exists_and_is_readable() {
   fi
 }
 
+function convert_filesystem_path_to_file_url() {
+  # Converts "~", "~/...", or an absolute filesystem path into an
+  # encoded file URL. The path does not need to exist.
+  #
+  # Usage:
+  #   file_url="$(convert_filesystem_path_to_file_url "~/Team Files")"
+  #
+  #   The output will be: 'file:///Users/tom/Team%20Files'
+  
+  report_start_phase_standard
+
+  local filesystem_path="$1"
+  local expanded_path
+
+  case "$filesystem_path" in
+    "~")
+      expanded_path="$HOME"
+      ;;
+
+    "~/"*)
+      expanded_path="$HOME/${filesystem_path#\~/}"
+      ;;
+
+    "~"*)
+      report_fail "Unsupported filesystem path '$filesystem_path': ~user syntax is not supported."
+      return 1
+      ;;
+
+    /*)
+      expanded_path="$filesystem_path"
+      ;;
+
+    *)
+      report_fail "Unsupported filesystem path '$filesystem_path': expected ~, ~/, or an absolute path."
+      return 1
+      ;;
+  esac
+
+  # Print result to standard out
+  jq -nr --arg path "$expanded_path" '
+    $path
+    | @uri
+    | gsub("%2F"; "/")
+    | "file://" + .
+  '
+  report_end_phase_standard
+}
+
 function get_array_of_2_tuples_from_json_file() {
   # Reads a top-level JSON array and returns each element as one compact
   # JSON value in the conventional zsh $reply array.
@@ -49,7 +97,7 @@ function get_array_of_2_tuples_from_json_file() {
 
   reply=()
 
-  output="$(jq -c '.[]' "$file_to_read")" || return 1
+  output="$(jq -c '.[]' "$file_to_read")"
 
   if [[ -n "$output" ]]; then
     reply=("${(@f)output}")
